85ff7e396f1b884742df3640c20e479598259550
lukecwik776 <102084230+lukecwik776@users.noreply.github.com>
2022-05-26 02:02:23 -0500

    Added and AddEmployerViewModel for validation, edited the employerController methods and corresponding views

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,24 +14,43 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+
+            return View(employers);
         }
 
-        public IActionResult Add()
+        public IActionResult Add(AddEmployerViewModel viewModel)
         {
-            return View();
+            return View("Add", viewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        [HttpPost]
+        public IActionResult ProcessAddEmployerForm(Employer employer)
         {
-            return View();
+
+            if (ModelState.IsValid)
+            {
+                context.Employers.Add(employer);
+                context.SaveChanges();
+                return Redirect("Add");
+            }
+            return View("Add", employer);
         }
 
         public IActionResult About(int id)
         {
+            ViewBag.employer = context.Employers.Find(id);
+
             return View();
         }
     }
---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
